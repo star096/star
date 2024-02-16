a99985a4b3d80cdfd192e7420e712ead265da310
star096 <157231870+star096@users.noreply.github.com>
2024-02-16 22:57:28 +0200

    Add files via upload

---
+++ modal.js
@@ -0,0 +1,15 @@
+const modal = document.querySelector('.backdrop');
+const modalBtnOpen = document.querySelectorAll('.modal-btn-open');
+const modalBtnClose = document.querySelectorAll('.modal-btn-close');
+
+const toggleModal = () => modal.classList.toggle('is-hidden');
+
+modalBtnOpen.forEach(function (e) 
+{
+    e.addEventListener('click',toggleModal);
+});
+
+modalBtnClose.forEach(function(e)
+{
+    e.addEventListener('click',toggleModal);
+});
